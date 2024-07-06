c5dda84c04344decb0e08ea33bebc946bd22e554
Mohd Salim <saalim9965@gmail.com>
2024-07-06 15:55:55 +0530

    section 6: lecture 115 dynamic and conditional styling with styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -16,19 +16,19 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${(props) => (props.$invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f7373f" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -56,25 +56,22 @@ export default function AuthInputs() {
         {/* paragraph class is added but it will not take into effect and this class is scoped to the Header component*/}
         <p className="paragraph">
           {/* also forwards all props (className in this case) to the underlying HTML element label in this case */}
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </Label>
+          {/* prop names should be different from the built in props invalid is a build in prop for label in HTML hence change the name to $invalid*/}
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
+            $invalid={emailNotValid}
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
             // }}
-            className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>
-            Password
-          </Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
